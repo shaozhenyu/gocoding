28bed7862290be0fd656cfb17e68a81fd1bf08b7
shaozhenyu <18217258834@163.com>
2017-03-09 22:56:40 +0800

    fix server

--- net/server.go
+++ net/server.go
@@ -8,6 +8,12 @@ import (
 	"os"
 )
 
+type User struct {
+	Name string
+	Ip   string
+	Data chan string
+}
+
 func main() {
 	ln, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
@@ -15,21 +21,38 @@ func main() {
 	}
 	defer ln.Close()
 
+	user := map[string]*User{}
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		ip := conn.RemoteAddr().String()
 		fmt.Println("connect : ", conn.RemoteAddr().String())
-
-		go handleConnection(conn)
+		if _, ok := user[ip]; !ok {
+			newOne := &User{
+				Name: ip,
+				Ip:   ip,
+				Data: make(chan string, 1024),
+			}
+			user[ip] = newOne
+		}
+		go handleConnection(conn, user[ip], user)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, user *User, all map[string]*User) {
 	var ch chan int = make(chan int)
 	r := bufio.NewReader(os.Stdin)
+
+	//client to client
+	go func() {
+		msg := <-user.Data
+		conn.Write([]byte(msg))
+	}()
+
 	go func() {
 		for {
 			data, _, _ := r.ReadLine()
@@ -49,6 +72,12 @@ func handleConnection(conn net.Conn) {
 		if err != nil {
 			ch <- 1
 			log.Fatal(err)
+		}
+		//send to someone
+		for k, v := range all {
+			if k != user.Ip {
+				v.Data <- string(buf)
+			}
 		}
 		fmt.Println(string(buf))
 	}
