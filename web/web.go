62253d9b4da31b99056276073f57cdd640ccd5ca
shaozhenyu <18217258834@163.com>
2017-04-11 22:06:40 +0800

    add

---
+++ web/web.go
@@ -0,0 +1,42 @@
+package main
+
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Router struct {
+	Name string
+}
+
+func (r *Router) ServeHTTP(http.ResponseWriter, *http.Request) {
+	fmt.Println("new route")
+}
+
+func New() *Router {
+	return &Router{
+		Name: "aa",
+	}
+}
+
+func main() {
+	fmt.Println("test")
+
+	router := New()
+
+
+	http.HandleFunc("/hello", Hello0)
+	http.HandleFunc("/hello1", Hello1)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Hello1(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello1111111")
+}
+
+func Hello0(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello00000000")
+}
