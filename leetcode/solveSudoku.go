c40730752ebc5f9d77f690299abde291a4efb0c0
shaozhenyu <18217258834@163.com>
2018-09-05 17:26:47 +0800

    add leetcode

--- leetcode/solveSudoku.go
+++ leetcode/solveSudoku.go
@@ -7,60 +7,68 @@ func main() {
 	solveSudoku(val)
 }
 
-var (
-	all = []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-)
 
 func solveSudoku(board [][]byte) {
-	a := make([]map[byte]bool, 9)
-	b := make([]map[byte]bool, 9)
-	c := make([]map[byte]bool, 9)
+	a := make([][]bool, 9)
+	b := make([][]bool, 9)
+	c := make([][]bool, 9)
 	for i := 0; i < 9; i++ {
-		a[i] = make(map[byte]bool)
-		b[i] = make(map[byte]bool)
-		c[i] = make(map[byte]bool)
+		a[i] = make([]bool, 9)
+		b[i] = make([]bool, 9)
+		c[i] = make([]bool, 9)
 	}
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
 			if board[i][j] != '.' {
-				a[i][board[i][j]] = true
-				b[j][board[i][j]] = true
-				c[i/3*3+j/3][board[i][j]] = true
+				a[i][board[i][j]-'1'] = true
+				b[j][board[i][j]-'1'] = true
+				c[i/3*3+j/3][board[i][j]-'1'] = true
 			}
 		}
 	}
-	backtrace(board, a, b, c, 0, 0)
+	backtrace(board, a, b, c, 0)
+	fmt.Println("after: ", board)
 }
 
-func backtrace(board [][]byte, a, b, c []map[byte]bool, m, n int) bool {
-	for i := m; i < 9; i++ {
-		for j := n; j < 9; j++ {
-			if board[i][j] != '.' {
-				continue
+var (
+	all = []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+)
+func backtrace(board [][]byte, a, b, c [][]bool, index int) bool {
+	if index == 81 {
+		return true
+	}
+	var start int
+	for start = index; start < 81; start++ {
+		i, j := start/9, start%9
+		if board[i][j] == '.' {
+			break
+		}
+	}
+	if start >= 81 {
+		return true
+	}
+	i, j := start/9, start%9
+	for x := 0; x < 9; x++ {
+		v := all[x] - '1'
+		if !a[i][v] && !b[j][v] && !c[i/3*3+j/3][v] {
+			board[i][j] = all[x]
+			a[i][v] = true
+			b[j][v] = true
+			c[i/3*3+j/3][v] = true
+			p := i
+			q := j + 1
+			if q == 9 {
+				q = 0
+				p++
 			}
-			for x := 0; x < 9; x++ {
-				v := all[x]
-				if !a[i][v] && !b[j][v] && !c[i/3*3+j/3][v] {
-					board[i][j] = v
-					a[i][v] = true
-					b[j][v] = true
-					c[i/3*3+j/3][v] = true
-					q := j + 1
-					p := i
-					if q == 9 {
-						q = 0
-						p++
-					}
-					ok := backtrace(board, a, b, c, p, q)
-					if ok {
-						return ok
-					}
-					board[i][j] = '.'
-					a[i][v] = false
-					b[j][v] = false
-					c[i/3*3+j/3][v] = false
-				}
+			ok := backtrace(board, a, b, c, start+1)
+			if ok {
+				return true
 			}
+			board[i][j] = '.'
+			a[i][v] = false
+			b[j][v] = false
+			c[i/3*3+j/3][v] = false
 		}
 	}
 	return false
